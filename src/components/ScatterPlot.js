ea169bdfece78e29ea37d138252f8d57595c1509
Saksham <sakshamag.work@gmail.com>
2022-09-04 00:26:49 +0530

    Fixed scatter plot

--- src/components/ScatterPlot.js
+++ src/components/ScatterPlot.js
@@ -1,42 +1,28 @@
 import React from 'react';
 import ReactEcharts from "echarts-for-react";
+import Data from '../Wine-Data.json'; // Data-Set
 
 function ScatterPlot() {
+
+    // Extracting Hue value from dataset into new array
+    const hue = Data.map(a => a.Hue);
+    // Extracting Color Intensity value from dataset into new array
+    const colorIntensity = Data.map(({ ColorIntensity }) => ColorIntensity)
+
     const option = {
         xAxis: {
             type: 'category',
             name: 'Color Intensity' // X-Axis Label name
         },
         yAxis: {
-            type: 'value',
+            type: 'category',
             name: 'Hue' // Y-Axis Label name
         },
         series: [
             {
                 symbolSize: 7,
                 data: [
-                    [10.0, 8.04],
-                    [8.07, 6.95],
-                    [13.0, 7.58],
-                    [9.05, 8.81],
-                    [11.0, 8.33],
-                    [14.0, 7.66],
-                    [13.4, 6.81],
-                    [10.0, 6.33],
-                    [14.0, 8.96],
-                    [12.5, 6.82],
-                    [9.15, 7.2],
-                    [11.5, 7.2],
-                    [3.03, 4.23],
-                    [12.2, 7.83],
-                    [2.02, 4.47],
-                    [1.05, 3.33],
-                    [4.05, 4.96],
-                    [6.03, 7.24],
-                    [12.0, 6.26],
-                    [12.0, 8.84],
-                    [7.08, 5.82],
-                    [5.02, 5.68]
+                    ...colorIntensity, ...hue // Used spread operator to provide values to be visualized
                 ],
                 type: 'scatter'
             }
